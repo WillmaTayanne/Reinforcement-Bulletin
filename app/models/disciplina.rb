8d0a3ca155762045cf8f3def15e69e9c0e45fdb2
lukkerr <luciano_carvalho@outlook.com.br>
2022-07-03 16:18:32 -0300

    ajuste em disciplina

--- app/models/disciplina.rb
+++ app/models/disciplina.rb
@@ -1,7 +1,6 @@
 class Disciplina < ApplicationRecord
     validates :cpf_professor, presence: true
     validates :nome_disciplina, presence: true
-    validates :ativo, presence: true
 
     belongs_to :usuarios, class_name: "Usuario", :foreign_key => :cpf_professor, primary_key: 'cpf'
     has_many :cursas, class_name: "Cursa", :foreign_key => :id_disciplina, primary_key: 'id_disciplina'
